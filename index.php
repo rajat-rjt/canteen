3b3b4d72ea8f0335df2c82a8d79780a08c6e175d
rajat-rjt <55527330+rajat-rjt@users.noreply.github.com>
2020-07-18 08:10:56 +0530

    login

---
+++ index.php
@@ -0,0 +1,157 @@
+</php
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Canteen</title>
+<!-- 
+Cafe House Template
+http://www.templatemo.com/tm-466-cafe-house
+-->
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,600,700' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Damion' rel='stylesheet' type='text/css'>
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/font-awesome.min.css" rel="stylesheet">
+  <link href="css/templatemo-style.css" rel="stylesheet">
+  <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon"/>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+  <body>
+
+    <div class="tm-top-header">
+      <div class="container">
+        <div class="row">
+          <div class="tm-top-header-inner">
+            <div class="tm-logo-container">
+              <img src="img/logo.png" alt="Logo" class="tm-site-logo" width="50px" height="50px">
+              <h1 class="tm-site-name tm-handwriting-font">Canteen</h1>
+            </div>
+            <div class="mobile-menu-icon">
+              <i class="fa fa-bars"></i>
+            </div>
+            <nav class="tm-nav">
+              <ul>
+                <li><a href="index.html" class="active">Home</a></li>
+                <li><a href="menu.html">Menu</a></li>
+                <li><a href="contact.html">Contact</a></li>
+                <li><a href="cart.html">Cart</a></li>
+              </ul>
+            </nav>   
+          </div>           
+        </div>    
+      </div>
+    </div>
+    <section class="tm-welcome-section">
+      <div class="container tm-position-relative">
+        <div class="tm-lights-container">
+          <img src="img/light.png" alt="Light" class="light light-1">
+          <img src="img/light.png" alt="Light" class="light light-2">
+          <img src="img/light.png" alt="Light" class="light light-3">  
+        </div>        
+        <div class="row tm-welcome-content">
+          <h2 class="white-text tm-handwriting-font tm-welcome-header"><img src="img/header-line.png" alt="Line" class="tm-header-line">&nbsp;Welcome To&nbsp;&nbsp;<img src="img/header-line.png" alt="Line" class="tm-header-line"></h2>
+          <h2 class="gold-text tm-welcome-header-2">Canteen</h2>
+          <p class="gray-text tm-welcome-description">Canteen template is a mobile-friendly responsive <span class="gold-text">Bootstrap v3.3.5</span> layout by <span class="gold-text">templatemo</span>. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculusnec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.</p>
+               
+        </div>
+        <img src="img/table-set.png" alt="Table Set" class="tm-table-set img-responsive">             
+      </div>      
+    </section>
+    <div class="tm-main-section light-gray-bg">
+      <div class="container" id="main">
+        <section class="tm-section row">
+          <div class="col-lg-9 col-md-9 col-sm-8">
+            <h2 class="tm-section-header gold-text tm-handwriting-font">The Best Canteen for you</h2>
+            <h2>Canteen</h2>
+            <p class="tm-welcome-description">This is free HTML5 website template from <span class="blue-text">template</span><span class="green-text">mo</span>. Fndimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Ettiam sit amet orci eget eros faucibus tincidunt.</p>
+            
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-4 tm-welcome-img-container">
+            <div class="inline-block shadow-img">
+              <img src="img/images.jfif" alt="Image" class="img-circle img-thumbnail">  
+            </div>              
+          </div>            
+        </section>          
+        <section class="tm-section tm-section-margin-bottom-0 row">
+          <div class="col-lg-12 tm-section-header-container">
+            <h2 class="tm-section-header gold-text tm-handwriting-font"><img src="img/logo.png" alt="Logo" class="tm-site-logo" width="50px" height="50px"> Popular Items</h2>
+            <div class="tm-hr-container"><hr class="tm-hr"></div>
+          </div>
+          <div class="col-lg-12 tm-popular-items-container">
+            <form action="cart.php" method="GET"></form>
+              <div class="tm-popular-item">
+                <img src="img/popular-1.jpg" alt="Popular" class="tm-popular-item-img" width="286px" height="286px">
+                <div class="tm-popular-item-description">
+                  <h3 class="tm-handwriting-font tm-popular-item-title"><span class="tm-handwriting-font bigger-first-letter">m</span>aggie</h3><hr class="tm-popular-item-hr">
+                  <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque. sed ipsum.</p>
+                  <div class="order-now-container">
+                    <a href="menu.php" class="order-now-link tm-handwriting-font">Order Now</a>
+                  </div>
+                </div>              
+              </div>
+              <div class="tm-popular-item">
+                <img src="img/popular-2.jfif" alt="Popular" class="tm-popular-item-img"width="286px" height="286px">
+                <div class="tm-popular-item-description">
+                  <h3 class="tm-handwriting-font tm-popular-item-title"><span class="tm-handwriting-font bigger-first-letter">p</span>asta</h3><hr class="tm-popular-item-hr">
+                  <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque. sed ipsum.</p>
+                  <div class="order-now-container">
+                    <a href="menu.php" class="order-now-link tm-handwriting-font">Order Now</a>
+                  </div>
+                </div>              
+              </div>
+              <div class="tm-popular-item">
+                <img src="img/popular-3.webp" alt="Popular" class="tm-popular-item-img" width="286px" height="286px">
+                <div class="tm-popular-item-description">
+                  <h3 class="tm-handwriting-font tm-popular-item-title"><span class="tm-handwriting-font bigger-first-letter">s</span>amosa</h3><hr class="tm-popular-item-hr">
+                  <p>Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque. sed ipsum.</p>
+                  <div class="order-now-container">
+                    <a href="menu.php" class="order-now-link tm-handwriting-font">Order Now</a>
+                  </div>
+                </div>              
+              </div>
+            </form>
+          </div>          
+        </section>
+        
+
+        <section class="tm-section tm-section-margin-bottom-0 row">
+          <div class="col-lg-12 tm-section-header-container">
+            <h2 class="tm-section-header gold-text tm-handwriting-font"><img src="img/logo.png" alt="Logo" class="tm-site-logo" width="50px" height="50px"> Go to Menu</h2>
+            <div class="tm-hr-container"><hr class="tm-hr"></div>
+          </div>
+          <div class="container">
+            <a class="menu_page" href = "#">
+              <img src="./img/menu.jpg" alt="Menu">
+            </a>
+          </div>
+
+        </section>
+      </div>
+    </div> 
+    <footer>
+      
+        <div class="container">
+          <div class="row tm-copyright">
+           <p class="col-lg-12 small copyright-text text-center">Copyright &copy; 2020 MAIMT Canteen</p>
+         </div>  
+       </div>
+    
+   </footer> <!-- Footer content-->  
+
+ </body>
+ </html>
